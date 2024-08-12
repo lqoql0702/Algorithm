5049f22a140c1def2a83b1039c89fe7b03a3eb62
inyoung <lqoql0702@gmail.com>
2024-08-12 23:50:09 +0900

    ex12, ex13

---
+++ src/ex13.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ex13 {
+    public ArrayList<Integer> solution(int n, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for(int i=1; i<n; i++){
+            if(arr[i]>arr[i-1]) answer.add(arr[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        ex13 T = new ex13();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+        for(int x : T.solution(n, arr)){
+            System.out.print(x+" ");
+        }
+    }
+
+}
